ed46b48418968779907f5bbd89c9675fe7bdc7e1
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-24 04:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, Ada){
 katzDeliLine.push(Ada)
-return "Welcome + Ada +You are number + katzDeliLine.length + in line"
+return 'Welcome,' + 'Ada' +'You are number' + 'katzDeliLine.length' + 'in line'
 }
