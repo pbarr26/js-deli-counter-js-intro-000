509cba45be1ef2e98fb9d43f399d7964360cdab5
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-24 05:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-for(let i=0; i<katzDeliLine.length; i++ )
-
 if(katzDeliLine.length ===0) {
 return "There is nobody waiting to be served!"
 }
